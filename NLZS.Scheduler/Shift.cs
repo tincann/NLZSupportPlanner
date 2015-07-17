cf1f58ab30466420047caddffaa970808d4dd15e
Morten Asscheman <morten.asscheman@infi.nl>
2015-07-17 15:07:09 +0200

    wip

---
+++ NLZS.Scheduler/Shift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLZS.Scheduler
+{
+    public class Shift
+    {
+        public string Employee { get; set; }
+        public int TimeSlot { get; set; }
+    }
+}
